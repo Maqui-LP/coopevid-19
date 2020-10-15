923e8d636eea1979bbdab32c2f0d980db2081d3d
Maqui-LP <maqui-lp@outlook.com>
2020-10-14 22:19:21 -0300

    se agrega editar y eliminar usuario

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,8 +1,9 @@
-from flask import redirect, render_template, request, url_for, session, abort, jsonify
+from flask import redirect, render_template, request, url_for, session, abort, jsonify, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.user import User
 from app.helpers.auth import authenticated
 from app.db_sqlalchemy import db_sqlalchemy
+from datetime import datetime
 
 db = db_sqlalchemy
 
@@ -10,7 +11,6 @@ db = db_sqlalchemy
 def index():
     users = User.query.all()
     return render_template("user/index.html", users=users)
-
 
 def new():
     if not authenticated(session):
@@ -24,9 +24,7 @@ def new():
   #      abort(401)
 
    # conn = connection()
-    #User.create(conn, request.form)
-    
-
+    #User.create(conn, request.form)    
 
 def create():
     
@@ -39,4 +37,49 @@ def create():
     
     db.session.add(new_user)
     db.session.commit()
+    return redirect(url_for("user_index"))
+
+def update():
+    if not authenticated(session):
+        abort(401)
+    user_id = request.args.get("user_id")
+    data = request.form.to_dict()
+    print(data)
+    user2 = User.query.filter(User.email == data.get("email"), User.id != user_id).first()
+    if (user2 is not None):
+        flash("Ya existe un usuario con ese email")
+        return
+    
+    user2 = User.query.filter(User.username == data.get("username"), User.id != user_id).first()
+    if(user2 is not None):
+        flash("Ya existe un usuario con ese username")
+        return
+
+    data["updated_at"] = datetime.now()
+    user = User.query.filter(User.id == user_id).update(data)
+
+    db.session.commit()
+    
+    return redirect(url_for("user_index"))
+
+def edit():
+    if not authenticated(session):
+        abort(401)
+    
+    user_id = request.args.get("user_id")
+    user = User.query.filter(User.id == user_id).first()
+
+    return render_template("user/update.html", user=user)
+
+
+def delete():
+    user_id = request.args.get("user_id")
+    user = User.query.filter(User.id == user_id).first()
+    if(user is None):
+        flash("El usuario no existe")
+        return
+
+    db.session.delete(user)
+    db.session.commit()
+    
     return redirect(url_for("user_index"))
