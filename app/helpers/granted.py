fa00afa2ba53dbc2cab1b194b51d09534be339a0
Maqui-LP <maqui-lp@outlook.com>
2020-10-17 19:22:59 -0300

    vistas navbar, opciones de usuario, update

--- app/helpers/granted.py
+++ app/helpers/granted.py
@@ -2,9 +2,11 @@ from flask import session
 from app.models.user import User
 
 def granted(permiso):
-
+    print("*************************************************")
+    print(session.get("user"))
     user = User.query.filter(User.id == session.get("user")).first()
-
+    if(user is None):
+        return False
     return permiso in User.getPermisos(user)
 
     
