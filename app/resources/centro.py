4037c45ae1761b5173b5486ca484d75e98c06cdd
GuillermoCalderaro <guillermocalderaro@gmail.com>
2020-10-28 20:48:25 -0300

    listado de centros

--- app/resources/centro.py
+++ app/resources/centro.py
@@ -12,21 +12,6 @@ from app.models.configuracion import Configuracion
 
 db = db_sqlalchemy
 
-class centro:  
-    def __init__(self, nombre, direccion, telefono):  
-        self.nombre = nombre  
-        self.direccion = direccion
-        self.telefono = telefono
-
-centros = []  
-
-centros.append( centro('Iglesia Sagrado Corazón de Jesús', 'Diagonal 73 nro 1032', '221-5139019'))
-centros.append( centro('Merendero Todos por una Sonrisa','Calle 88 nro 1912, Altos de San Lorenzo','221 - 5930941'))
-centros.append( centro('La casita de Niquin', 'Calle 2 nro 3423', '221-754853'))
-centros.append( centro('Pandemias eran las de antes','Calle 33 nro 787','221 - 7546534'))
-centros.append( centro('Viva el Ejercito Popular Chino', 'Chinchin 123', '221-5139019'))
-centros.append( centro('Dos mil gracias','Calle 88 nro 1912, Tolosa','221 - 56475385'))
-
 
 
 def new():
@@ -41,6 +26,8 @@ def new():
 def index():
     if not authenticated(session):
         abort(401)
+
+    centros = Centro.getAll()
 
     return render_template("centro/index.html", centros=centros)
 
