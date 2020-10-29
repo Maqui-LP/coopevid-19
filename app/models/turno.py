cc4b4db7ae0a803870a82cc406b7f0a4b588e0ee
maquipoisson <maquipoisson@gmail.com>
2020-10-29 11:48:43 -0300

    cambio tipo hora

--- app/models/turno.py
+++ app/models/turno.py
@@ -1,5 +1,6 @@
 from app.db_sqlalchemy import db_sqlalchemy
-from datetime import date, datetime
+from datetime import date
+from sqlalchemy.dialects.mysql import TIME
 
 db = db_sqlalchemy
 
@@ -7,6 +8,6 @@ class Turno(db.Model):
     __tablename__ = 'turno'
     id = db.Column(db.Integer , primary_key = True)
     dia = db.Column(db.Date, unique=False, nullable=False)
-    horaInicio = db.Column(db.DateTime, nullable=False)
+    horaInicio = db.Column(TIME(), unique= False, nullable=False)
     userEmail = db.Column(db.String(255), nullable=False)
     userId = db.Column(db.Integer, db.ForeingKey('users.id'))
