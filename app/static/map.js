62b62032810a0b3148ca6dde9cabee8ca8203c89
Rodriguez Boschi,Nicolas <nicolasrodriguezboschi@gmail.com>
2020-11-02 20:08:43 -0300

    se desacoplo el codigo js del html de centro

--- app/static/map.js
+++
@@ -1,7 +0,0 @@
-let myMap = L.map('myMap').setView([0,0],2)
-
-const urlOpenLayers = 'https://a.tile.openstreetmap.org/${z}/${x}/${y}.png'
-
-L.tileLayer(urlOpenLayers,{
-    maxZoom:15
-}).addTo(myMap)
