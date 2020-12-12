c045b4f3cfc58ac8ffe141591ca87346abe7c1c0
Lindo Poisson, Macarena <maquipoisson@gmail.com>
2020-12-12 12:59:31 -0300

    Se agrega sacar turno

--- web/src/views/SacarTurno.vue
+++ web/src/views/SacarTurno.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="sacarTurno">
     <h1>Sacar Turno</h1>
-
+    <FormularioSacarTurno />
   </div>
 </template>
 
 <script>
+
+import FormularioSacarTurno from '@/components/FormularioSacarTurno.vue'
+
 export default {
-  data() {
-    return {
-      data: [],
-      errors: []
-    }
-  }
+  name:'SacarTurno',
+  components: { FormularioSacarTurno },
 }
+
 </script>
