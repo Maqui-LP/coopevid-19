97a4bc2ea72b0bf5dbfd2b3ee6eb071869c4447e
GuillermoCalderaro <guillermocalderaro@gmail.com>
2020-12-18 20:10:11 -0300

    inclusion de v-charts y primeros graficos

---
+++ web/src/components/EstadisticasPorTipoCentro.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container card">
+        <h3>Porcentaje de centros por tipo de donaciones</h3>
+      <ve-pie :data="chartData" :settings="chartSettings"></ve-pie>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      this.chartSettings = {
+        roseType: 'radius'
+      }
+      return {
+        chartData: {
+          columns: ['date', 'cost'],
+          rows: [
+            { 'date': 'Comida', 'cost': 20 },
+            { 'date': 'Ropa', 'cost': 10 },
+            { 'date': 'Higiene Personal', 'cost': 10 },
+            { 'date': 'Higiene del Hogar', 'cost': 30 },
+            { 'date': 'Muebles', 'cost': 30 },
+          ]
+        }
+      }
+    }
+  }
+</script>
